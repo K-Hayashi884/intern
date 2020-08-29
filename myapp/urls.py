8255654398974fd69895da6664c5f4781d318f6d
ISSEI-HAYASHI <issei884money@gmail.com>
2020-08-30 02:46:30 +0900

    login 実装

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth import logout
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('signup', views.signup, name='signup'),
-    path('login', views.login, name='login'),
+    path('signup', views.signup_view, name='signup_view'),
+    path('login', views.login_view, name='login_view'),
     path('friends', views.friends, name='friends'),
     path('talk_room', views.talk_room, name='talk_room'),
     path('setting', views.setting, name='setting'),
--- myapp/views.py
+++ myapp/views.py
@@ -4,12 +4,14 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from .forms import SignUpForm,LoginForm
+from django.contrib import messages 
+
 # Create your views here.
 from .models import User,UserImage
 def index(request):
     return render (request, "myapp/index.html")
 
-def signup(request):
+def signup_view(request):
     if request.method == "GET":
         form = SignUpForm()
         params = {"form":form,}
@@ -36,25 +38,24 @@ def signup(request):
         params = {"form":form,}
         return render(request,"myapp/signup.html",params)
         
-def login(request,*user):
+def login_view(request):
     if request.method == "GET":
         form = LoginForm()
         params = {"form":form}
         return render (request, "myapp/login.html",params)
     elif request.method == "POST":
-        print("POST")
-        print(request.POST)
         form = LoginForm(request.POST)
-        if form.is_valid():
-            print('form.is_valid')
-            username = form.cleaned_data.get('username')
-            user = User.objects.get(username=username)
-            login(request, user)
-            return redirect('/')
-        else:
-            print('else')
-        params = {"form":form}
-        return render(request,"myapp/login.html",params)
+        username = request.POST['username'] 
+        password = request.POST['password'] 
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect("/") 
+        else: 
+            messages.error(request,'username or password not correct') 
+            params = {"form":form}
+            return render(request,"myapp/login.html",params)
 
 def friends(request):
     return render (request, "myapp/friends.html")
