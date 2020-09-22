aef80770f12807fe9fda28b7bfc9f4979a3c4121
K-Hayashi884 <hayakake2000@gmail.com>
2020-09-22 23:51:23 +0900

    urls

--- intern/urls.py
+++ intern/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import myapp.views as myapp
+from django.contrib.staticfiles.urls import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
 ]
+
+urlpatterns += static(setting.STATIC_URL, document_root=settings.STATIC_ROOT)
