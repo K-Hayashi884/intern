1c8cea5eab8caa0310cccd56319995a72b862c3e
shunki <bonoshunki@gmail.com>
2020-09-27 17:15:58 +0900

    ku

--- myapp/views.py
+++ myapp/views.py
@@ -50,7 +50,6 @@ def talklist_view(request):
            # htmlで表示するにあたって必要な情報を紐づけたリストを作成する
            # ※※時間のソートをかける際に、0やnullでは型が違ってsortできない
             friend_info.append([friend, last_message, time_flag, datetime.datetime.now(2020, 9, 27, 17, 00)])
-            # 最後の要素（＝そのトークのtime）でソートすることで、html上の組み込みでforを回すだけで最新から順に表示することができる
     friend_info = sorted(friend_info, reverse=True, key=lambda x: x[3])
     params = {
         "user_imgs":user_imgs,
