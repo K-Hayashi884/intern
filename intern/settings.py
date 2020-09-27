e0cf610ec3448103ba47bb6e8065d5faf20fcab4
shunki <bonoshunki@gmail.com>
2020-09-27 16:06:39 +0900

    u

--- intern/settings.py
+++ intern/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'oaab#2kr%trbj2h-w9ycf0&f$7dgi2+p=37!cjw$*y0@26pq77'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["54.150.183.122"]
 
 
 # Application definition
