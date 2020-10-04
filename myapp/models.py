f54b5d068a135b3e974d801c6d03b7893c9e374e
yusuketosa <y.tosa.kyoto@gmail.com>
2020-10-04 21:34:02 +0900

    models修正

--- myapp/models.py
+++ myapp/models.py
@@ -1,10 +1,10 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.db import models
 from django.contrib.auth.models import AbstractUser,UserManager
 from django.utils.translation import gettext_lazy as _
 import datetime
 
-class User(AbstractUser):
+class User(AbstractUser, PermissionsMixin):
     pass
 
 class UserImage(models.Model):
