3522e28092748655f8b9f063a6056181c3edc292
yusuketosa <y.tosa.kyoto@gmail.com>
2020-10-04 21:46:38 +0900

    models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,18 +1,18 @@
-from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
-from django.contrib.auth.models import AbstractUser,UserManager
-from django.utils.translation import gettext_lazy as _
-import datetime
 
-class User(AbstractUser, PermissionsMixin):
+
+class User(AbstractUser):
     pass
 
-class UserImage(models.Model):
 
-    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name="user_img")
-    image = models.ImageField(verbose_name="画像",upload_to="images")
+class UserImage(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user_img")
+    image = models.ImageField(verbose_name="画像", upload_to="images")
+
     def __str__(self):
         return "{}の写真".format(self.user)
+
 
 # トーク内容を全てdatbaseに保存する形をとる
 # ＞１個のトーク内容に紐づける情報は
@@ -22,13 +22,14 @@ class UserImage(models.Model):
 # という情報
 class Talk(models.Model):
     # メッセージ
-    talk = models.CharField(max_length = 500)
+    talk = models.CharField(max_length=500)
     # 誰から
     talk_from = models.ForeignKey(User, on_delete=models.CASCADE, related_name="talk_from")
     # 誰に
     talk_to = models.ForeignKey(User, on_delete=models.CASCADE, related_name="talk_to")
     # 時間は
-    time = models.DateTimeField(null=True)
-    def __int__(self):
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
         return "{}>>{}".format(self.talk_from, self.talk_to)
 
