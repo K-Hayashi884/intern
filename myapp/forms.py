bc3c1229ffeef74fddf1e035cc3822d51ba05453
inomoto <hiroto_inomoto@demia.co.jp>
2021-06-01 18:03:22 +0900

    firstcommit

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,1 +1,22 @@
+from .models import User
+from django import forms
 
+class SignUpForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    image = forms.ImageField(required=True)
+    class Meta:
+        model = User
+        fields = ['name', 'mail', 'password','image']
+        widgets = {
+            'name': forms.TextInput(attrs={'class':'form-control'}),
+            'mail': forms.EmailInput(attrs={'class':'form-control'}),
+        }
+#ログイン
+class LogInForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    class Meta:
+        model = User
+        fields = ['name', 'password']
+        widgets = {
+            'name': forms.TextInput(attrs={'class':'form-control'}),
+        }
--- myapp/views.py
+++ myapp/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import redirect, render
-
+from .models import User
+from .forms import SignUpForm, LogInForm
+#ログイン機能
+from django.contrib.auth import authenticate, login
 
 def index(request):
-    return render(request, "myapp/index.html")
+    params = {
+      'title': '¡Hala Madrid!',    
+      }
+    return render(request, "myapp/index.html", params)
 
 def signup_view(request):
-    return render(request, "myapp/signup.html")
+    params = {
+        'title':'会員登録',
+        'form': SignUpForm(),
+        'message':'',
+    }
+    if (request.method == 'POST'):
+        obj = User()
+        form = SignUpForm(request.POST, request.FILES, instance=obj)
+        
+        if  (form.is_valid()):
+            form.save()
+            return redirect(to='/accounts/login')
+            #https://techpr.info/python/django-loginfunction/
+            #重複ログイン排除したい。
 
-def login_view(request):
-    return render(request, "myapp/login.html")
+        else:
+            params['message'] = '不備があります。これ今全部小文字じゃないとだめになってるから追って修正する'
+   
+    return render(request, "myapp/signup.html", params)
 
 def friends(request):
     return render(request, "myapp/friends.html")
@@ -18,3 +39,27 @@ def talk_room(request):
 
 def setting(request):
     return render(request, "myapp/setting.html")
+
+#ログインの設定
+def login_view(request):
+    if request.method == 'POST':
+        obj = User
+        form = LogInForm(request.POST, instance=obj)
+
+        # usernameを指定します。
+        # emailを使用したい場合は、Userモデルをカスタマイズする必要があります
+        username = request.POST['username']
+        print(username)
+        password = request.POST['password']
+        if form.is_valid():
+            # DBに存在するか確認
+            user = authenticate(request, username=username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                return redirect('talk_room')
+            else:
+                # 認証失敗時
+                return redirect('login_view')
+
+    return render(request, 'myapp/login.html')
