ed09ee977d0ab56860c911d3d800d8793ba2f15c
2212996 <51160826+2212996@users.noreply.github.com>
2021-06-09 15:36:37 +0900

    いろいろ

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import redirect, render
 
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+from django.views.generic import CreateView
+from . forms import UserCreateForm
+
 def index(request):
     return render(request, "myapp/index.html")
 
@@ -18,3 +23,23 @@ def talk_room(request):
 def setting(request):
     return render(request, "myapp/setting.html")
 
+#アカウント作成
+class Create_account(CreateView):
+    def post(self, request, *args, **kwargs):
+        form = UserCreateForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            #フォームから'username'を読み取る
+            username = form.cleaned_data.get('username')
+            #フォームから'password1'を読み取る
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/')
+        return render(request, 'myapp/signup.html', {'form': form,})
+
+    def get(self, request, *args, **kwargs):
+        form = UserCreateForm(request.POST)
+        return  render(request, 'myapp/signup.html', {'form': form,})
+
+create_account = Create_account.as_view()
