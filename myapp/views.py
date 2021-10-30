19232ba10a96a6fda9319c0ef326caa307f1b662
nekonekko <momoichigodayo+github@gmail.com>
2021-10-30 21:26:42 +0900

    50% of signup

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import redirect, render
+from .models import User
+from .forms import SignupForm
 
 
 def index(request):
     return render(request, "myapp/index.html")
 
 def signup_view(request):
-    return render(request, "myapp/signup.html")
+    if request.method == "POST":
+        form = SignupForm(request.POST, request.FILES, instance=User())
+        if form.is_valid():
+            form.save()
+            return redirect(to='/')
+        else:
+            print('ERROR!!!')
+    return render(request, "myapp/signup.html", {'form': SignupForm()})
 
 def login_view(request):
     return render(request, "myapp/login.html")
