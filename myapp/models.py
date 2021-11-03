09ee2ef55c3d4b03d545cc99f968c9caf55f65f2
SekiKensuke <seki.kensuke.88w@st.kyoto-u.ac.jp>
2021-11-03 18:49:46 +0900

    migrationed

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.forms.fields import ImageField
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    image = models.ImageField(upload_to='icon/')
+
+    def __str__(self):
+        return self.username + ' ' + self.password
