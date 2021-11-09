a215668b615dc305719383bf3e0118143eaa1ce4
Ryunosuke <ryu32349581@gmail.com>
2021-11-10 00:08:43 +0900

    signupの実装

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,1 +1,33 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
+class SignUpForm(UserCreationForm):
+    image=forms.ImageField()
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.fields["username"].widget.attrs.update({
+            'class':"shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline",
+            'placeholder':""
+        })
+        self.fields["email"].widget.attrs.update({
+            'class':"shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline",
+            'placeholder':""
+        })
+        self.fields["password1"].widget.attrs.update({
+            'class':"shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline",
+            'placeholder':""
+        })
+        self.fields["password2"].widget.attrs.update({
+            'class':"shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline",
+            'placeholder':""
+        })
+
+#     # name=forms.CharField(label='Username',widget=forms.TextInput(attrs={'class':'form-control mb-4'}))
+#     # mail=forms.EmailField(label='Email address',widget=forms.TextInput(attrs={'class':'form-control mb-4'}))
+#     # password1=forms.CharField(label="Password",widget=forms.PasswordInput(attrs={'class':'form-control mb-4'}))
+#     # password2=forms.CharField(label="Password confirmation",widget=forms.PasswordInput(attrs={'class':'form-control mb-4'}))
+#     # image=forms.ImageField(label="img",widget=forms.FileInput(attrs={'class':'mb-4'}))
+    class Meta:
+        model=User
+        fields=['username','email','password1','password2',"image"]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import redirect, render
-
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 
 def index(request):
     params={
@@ -9,7 +11,18 @@ def index(request):
     return render(request, "myapp/index.html",params)
 
 def signup_view(request):
-    return render(request, "myapp/signup.html")
+    if request.method=="POST":
+        form=SignUpForm(request.POST)
+        if form.is_valid:
+            usename=form.cleaned_data.get("username")
+            password=form.cleaned_data.get("password1")
+            form.save()
+            new_user=authenticate(username=username,password=password)
+            if new_user is not None:
+                login(request,new_user)
+                redirect('index')
+    form=SignUpForm()
+    return render(request, "myapp/signup.html",{'form':form})
 
 def login_view(request):
     return render(request, "myapp/login.html")
