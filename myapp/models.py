98a59afe72bfc4a9e6f9c10474a3a83340b0834d
SekiKensuke <seki.kensuke.88w@st.kyoto-u.ac.jp>
2021-11-12 19:26:00 +0900

    all sevice are available

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import django.utils.timezone
 
 # Create your models here.
 
@@ -8,3 +9,9 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username + ' ' + self.password
+
+class Message(models.Model):
+    sender = models.CharField(max_length=150)
+    receiver = models.CharField(max_length=150)
+    contents = models.CharField(max_length=500)
+    date_joined = models.DateTimeField(default=django.utils.timezone.now)
