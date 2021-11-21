e005f74c82cefcee8ef8a60c464e4b31ceb1b7aa
rmago <r.magoshi.ma54@gmail.com>
2021-11-21 20:35:37 +0900

    settings modified

--- intern/settings.py
+++ intern/settings.py
@@ -124,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
