1fc9c8707c479cf368443bf25c740010c6cea508
Ryunosuke <ryu32349581@gmail.com>
2021-12-05 15:25:50 +0900

    before deploy

--- intern/settings.py
+++ intern/settings.py
@@ -124,8 +124,17 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL='myapp.Profile'
+
+# ...
+try:
+    from .local_settings import *
+except ImportError:
+    # local_settings.py が存在しなくてもエラーにならないようにする
+    pass
 
